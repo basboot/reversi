5bcc45ce7a3cf1107fa0e8f0429fe51844b716fe
Bas Boot <github@basboot.nl>
2023-01-10 10:31:14 +0100

    Update win count

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import random
 import time
 
-from balanced_player import balanced_player
 from greedy_player import greedy_player
 from patient_player import patient_player
 from random_player import random_player
 from reversi import *
 
+from alpha_zero import *
+from alpha_reversi import *
+
 if __name__ == '__main__':
     game = None
 
     tie = 0
+    player0 = 0
     player1 = 0
-    player2 = 0
 
-    players = [random_player, balanced_player]
+    alpha_zero = AlphaZero(AlphaReversi())
+
+    # Let op player 0 en 1 / maar WHITE = 1 en BLACK = 2! (zwart begint)
+    players = [patient_player, random_player]
+    # players = [random_player, random_player]
+
 
     start = time.time()
-    for i in range(1000):
-        game = Reversi()
-        first_player = random.randint(0, 1)
+    for i in range(100):
+        game = AlphaReversi()
+
+        # select player that plays with BLACK
+        first_player = 0#random.randint(0, 1)
 
         while True:
             moves = game.legal_moves()
@@ -46,18 +55,18 @@ if __name__ == '__main__':
         if result == 0:
             tie += 1
         else:
-            if result == 2: # white wins
+            if result == BLACK: # black wins == first player wins
                 if first_player == 0:
-                    player1 += 1
+                    player0 += 1
                 else:
-                    player2 += 1
+                    player1 += 1
             else:
                 if first_player == 0:
-                    player2 += 1
-                else:
                     player1 += 1
+                else:
+                    player0 += 1
 
-    print("P1, P2, TIE", player1, player2, tie)
+    print("P0, P1, TIE", player0, player1, tie)
 
     end = time.time()
     print("Time consumed in working: ", end - start)
