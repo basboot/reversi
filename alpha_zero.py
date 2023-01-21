2f761c21ad15e62d92c6cb197cf7559bc447c5d0
Bas Boot <github@basboot.nl>
2023-01-21 16:05:36 +0100

    Add nn training

--- alpha_zero.py
+++ alpha_zero.py
@@ -3,7 +3,7 @@ import os
 import random
 
 from mcts import MCTS
-from alpha_reversi import *
+from alpha_nim import *
 
 MODEL_PATH = "./models/"
 
@@ -21,6 +21,9 @@ class AlphaZero():
     # - simulate games
 
     def load_nn_values(self):
+        # TODO: change to tf format to also save the optimizer status
+        #  https://stackoverflow.com/questions/42666046/loading-a-trained-keras-model-and-continue-training
+
         # load model weights from disk, if exists
         filename = MODEL_PATH + self.alpha_game.network_name()
         print("LOAD: ", filename)
@@ -54,14 +57,41 @@ class AlphaZero():
         return self.best_move_from_policy(policy)
 
     # perform training iterations to fill the tree
-    def mcts_training(self):
-        pass
+    def mcts_training(self, game=None):
+        if game is None:
+            game = self.alpha_game
+
+        if game.get_id() not in self.mcts.nodes:
+            print("Gamestate not in MCTS, create new MCTS with this gamestate as root")
+            self.mcts = MCTS(game)
+
+        N_SIMULATIONS = 1000000
+        for n in range(N_SIMULATIONS):
+            self.mcts.simulate_game(self.mcts.nodes[game.get_id()])
 
     def mcts_extract_training_examples(self):
-        pass
+        N_SAMPLES = 100000
+        N_VALIDATION = 1000
 
-    def fit(self):
-        pass
+        # arrays with numpy objects in correct shape
+        x, y_p, y_v = self.mcts.create_training_samples(N_SAMPLES)
+        v_x, v_y_p, v_y_v = self.mcts.create_training_samples(N_VALIDATION)
+
+        print(len(x))
+
+        tf_x = tf.stack(x)
+        tf_y_p = tf.stack(y_p)
+        tf_y_v = tf.stack(y_v)
+
+        tf_v_x = tf.stack(v_x)
+        tf_v_y_p = tf.stack(v_y_p)
+        tf_v_y_v = tf.stack(v_y_v)
+
+        return tf_x, (tf_y_p, tf_y_v), tf_v_x, (tf_v_y_p, tf_v_y_v)
+
+    def nn_fit_training_examples(self):
+        x_train, y_train, x_validation, y_validation = self.mcts_extract_training_examples()
+        self.nn.fit(x_train, y_train, epochs=10, validation_data = (x_validation,y_validation))
 
     def player(self, game):
         # if game not in mcts, replace mcts
@@ -71,7 +101,7 @@ class AlphaZero():
 
         assert game.get_id() in self.mcts.nodes, "node not in mcts"
 
-        N_SIMULATIONS = 100
+        N_SIMULATIONS = 10
         for n in range(N_SIMULATIONS):
             self.mcts.simulate_game(self.mcts.nodes[game.get_id()])
 
@@ -84,8 +114,12 @@ class AlphaZero():
             return random.choice(game.legal_moves())
 
 if __name__ == '__main__':
-    game = AlphaReversi()
+    game = AlphaNim()
     alpha_zero = AlphaZero(game)
+
+    alpha_zero.mcts_training()
+
+    alpha_zero.nn_fit_training_examples()
 
     move = alpha_zero.player(game)
 
