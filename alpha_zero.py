defaa278c224c8f02d02d738c32ab2719cac2613
Bas Boot <github@basboot.nl>
2023-01-26 16:39:24 +0100

    Add configuration

--- alpha_zero.py
+++ alpha_zero.py
@@ -7,15 +7,19 @@ from alpha_nim import *
 
 MODEL_PATH = "./models/"
 
+N_SAMPLES = 100000
+N_VALIDATION = 1000
+N_SIMULATIONS = 1000000
 
 class AlphaZero():
-    def __init__(self, alpha_game):
+    def __init__(self, alpha_game, load_nn=True):
         self.alpha_game = alpha_game
         self.mcts = MCTS(self.alpha_game)
 
         self.nn = alpha_game.create_nn()
 
-        self.load_nn_values()
+        if load_nn:
+            self.load_nn_values()
 
     # TODO
     # - cleanup policy and normalize (alphazero)
@@ -54,10 +58,14 @@ class AlphaZero():
         # create policy from prediction and legal actions
         legal_actions = game.legal_moves()
         # p, _ = self.nn.predict(tf.reshape(game.full_gamestate, (1,) + self.alpha_game.input_dimension), verbose=0)
-        p, _ = self.nn(tf.reshape(game.full_gamestate, (1,) + self.alpha_game.input_dimension), training=False)
+        p, v = self.nn(tf.reshape(game.full_gamestate, (1,) + self.alpha_game.input_dimension), training=False)
 
+        print("p = ", p)
+        print("v = ", v)
         # one sample, one prediction
         policy = self.alpha_game.p_to_policy(p[0], legal_actions)
+
+        print("p_clean = ", policy)
 
         return self.best_move_from_policy(policy)
 
@@ -71,7 +79,7 @@ class AlphaZero():
         return v
 
     # perform training iterations to fill the tree
-    def mcts_training(self, game=None):
+    def mcts_training(self, game=None, n_simulations=None):
         if game is None:
             game = self.alpha_game
 
@@ -79,17 +87,15 @@ class AlphaZero():
             print("Gamestate not in MCTS, create new MCTS with this gamestate as root")
             self.mcts = MCTS(game)
 
-        N_SIMULATIONS = 1000000
-        for n in range(N_SIMULATIONS):
+
+        for n in range(n_simulations):
             self.mcts.simulate_game(self.mcts.nodes[game.get_id()])
 
-    def mcts_extract_training_examples(self):
-        N_SAMPLES = 100000
-        N_VALIDATION = 1000
+    def mcts_extract_training_examples(self, n_samples=N_SAMPLES, n_validation=N_VALIDATION):
 
         # arrays with numpy objects in correct shape
-        x, y_p, y_v = self.mcts.create_training_samples(N_SAMPLES)
-        v_x, v_y_p, v_y_v = self.mcts.create_training_samples(N_VALIDATION)
+        x, y_p, y_v = self.mcts.create_training_samples(n_samples)
+        v_x, v_y_p, v_y_v = self.mcts.create_training_samples(n_validation)
 
         # print(len(x))
 
@@ -103,11 +109,14 @@ class AlphaZero():
 
         return tf_x, (tf_y_p, tf_y_v), tf_v_x, (tf_v_y_p, tf_v_y_v)
 
-    def nn_fit_training_examples(self):
-        x_train, y_train, x_validation, y_validation = self.mcts_extract_training_examples()
+    def nn_fit_training_examples(self, n_samples=N_SAMPLES, n_validation=N_VALIDATION):
+        x_train, y_train, x_validation, y_validation = self.mcts_extract_training_examples(n_samples, n_validation)
         self.nn.fit(x_train, y_train, epochs=10, validation_data = (x_validation,y_validation))
 
-    def player(self, game, prediction_only=False, mcts_only=False, rollout=True):
+    def player(self, game, prediction_only=False, mcts_only=False, rollout=True, always_renew_mcts=True):
+
+        if always_renew_mcts:
+            self.mcts = MCTS(game)
 
         # prediction only = play without learning
         if not prediction_only:
@@ -119,7 +128,7 @@ class AlphaZero():
 
             assert game.get_id() in self.mcts.nodes, "node not in mcts"
 
-            N_SIMULATIONS = 100
+            N_SIMULATIONS = 10000
             for n in range(N_SIMULATIONS):
                 self.mcts.simulate_game(self.mcts.nodes[game.get_id()],
                                         predict_v_function=None if rollout else self.predict_v)
@@ -136,24 +145,29 @@ class AlphaZero():
                     return random.choice(game.legal_moves())
 
             # train nn from expanded tree
-            self.nn_fit_training_examples()
+            self.nn_fit_training_examples(n_samples=10000, n_validation=100)
 
         return self.predict_best_move(game)
 
 if __name__ == '__main__':
-    game = AlphaNim()
-    alpha_zero = AlphaZero(game)
+    game = AlphaNim([1, 2, 3, 4])
+    alpha_zero = AlphaZero(game, load_nn=False)
 
     alpha_zero.load_nn_values()
 
-    alpha_zero.mcts_training()
+    alpha_zero.mcts_training(game=None, n_simulations=10000)
 
-    alpha_zero.nn_fit_training_examples()
+    alpha_zero.nn_fit_training_examples(n_samples=10000, n_validation=100)
 
     alpha_zero.save_nn_values()
 
-    move = alpha_zero.player(game)
+    # move = alpha_zero.player(game)
 
-    print(alpha_zero.predict_best_move(game))
+    alpha_zero.predict_best_move(game)
 
-    print(move)
+    print("p_mcts", alpha_zero.mcts.root.policy())
+    print("v_mcts", alpha_zero.mcts.root.v / alpha_zero.mcts.root.n)
+
+    #alpha_zero.mcts.show()
+
+    # print(move)
